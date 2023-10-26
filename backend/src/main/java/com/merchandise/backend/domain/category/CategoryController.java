6b9c90469fc89f1512c33636ef9d9621a0b81067
Mohammad <anas4u02@gmail.com>
2023-10-26 11:14:00 +0530

    Created Controller for category
    Signed-off-by: Mohammad <anas4u02@gmail.com>

---
+++ backend/src/main/java/com/merchandise/backend/domain/category/CategoryController.java
@@ -0,0 +1,47 @@
+package com.merchandise.backend.domain.category;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+
+    private final CategoryServiceImpl categoryService;
+
+    public CategoryController(CategoryServiceImpl categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CategoryOutDto> findAll() {
+        return categoryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryOutDto findOne(@PathVariable("id") Long id) {
+        return categoryService.findOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryOutDto create(@RequestBody CategoryInDto categoryInDto) {
+        return categoryService.create(categoryInDto);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryOutDto update(@RequestBody CategoryInDto categoryInDto, @PathVariable("id") Long id) {
+        return categoryService.update(id, categoryInDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void delete(@PathVariable("id") Long id) {
+        categoryService.delete(id);
+    }
+}
