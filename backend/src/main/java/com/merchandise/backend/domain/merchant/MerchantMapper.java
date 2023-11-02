ccca6a6af8221b11790257d8d073fe99ed633f39
Mohammad <anas4u02@gmail.com>
2023-11-03 02:58:20 +0530

    Created Mapper class for merchant
    Signed-off-by: Mohammad <anas4u02@gmail.com>

---
+++ backend/src/main/java/com/merchandise/backend/domain/merchant/MerchantMapper.java
@@ -0,0 +1,54 @@
+package com.merchandise.backend.domain.merchant;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MerchantMapper {
+
+    public MerchantEntity inDtoToEntity(MerchantInDto merchantInDto) {
+        MerchantEntity merchantEntity = new MerchantEntity();
+        merchantEntity.setName(merchantInDto.getName());
+        merchantEntity.setEmail(merchantInDto.getEmail());
+        merchantEntity.setPhoneNumber(merchantInDto.getPhoneNumber());
+        merchantEntity.setAddress(merchantInDto.getAddress());
+        merchantEntity.setCity(merchantInDto.getCity());
+        merchantEntity.setState(merchantInDto.getState());
+        merchantEntity.setPinCode(merchantInDto.getPinCode());
+        merchantEntity.setUpiID(merchantEntity.getUpiID());
+        return merchantEntity;
+    }
+
+    public MerchantOutDto entityToOutDto(MerchantEntity merchantEntity) {
+        MerchantOutDto merchantOutDto = new MerchantOutDto();
+        merchantOutDto.setMerchantId(merchantEntity.getId());
+        merchantOutDto.setName(merchantEntity.getName());
+        merchantOutDto.setEmail(merchantEntity.getEmail());
+        merchantOutDto.setPhoneNumber(merchantEntity.getPhoneNumber());
+        merchantOutDto.setAddress(merchantEntity.getAddress());
+        merchantOutDto.setCity(merchantEntity.getCity());
+        merchantOutDto.setState(merchantEntity.getState());
+        merchantOutDto.setPinCode(merchantEntity.getPinCode());
+        merchantOutDto.setUpiId(merchantEntity.getUpiID());
+        return merchantOutDto;
+    }
+
+    public MerchantEntity inDtoToExistingEntity(MerchantInDto merchantInDto, MerchantEntity merchantEntity) {
+        merchantEntity.setName(merchantInDto.getName());
+        merchantEntity.setEmail(merchantInDto.getEmail());
+        merchantEntity.setPhoneNumber(merchantInDto.getPhoneNumber());
+        merchantEntity.setAddress(merchantInDto.getAddress());
+        merchantEntity.setCity(merchantInDto.getCity());
+        merchantEntity.setState(merchantInDto.getState());
+        merchantEntity.setPinCode(merchantInDto.getPinCode());
+        merchantEntity.setUpiID(merchantEntity.getUpiID());
+        return merchantEntity;
+    }
+
+    public List<MerchantOutDto> entitiesToOutDtos(List<MerchantEntity> merchantEntities) {
+        return merchantEntities.stream()
+                .map(this::entityToOutDto)
+                .toList();
+    }
+}
