f92fe49f2aa4f22792361c3bff798abd484ccbc8
Mohammad <anas4u02@gmail.com>
2023-11-03 02:56:24 +0530

    Created model class for merchant
    Signed-off-by: Mohammad <anas4u02@gmail.com>

---
+++ backend/src/main/java/com/merchandise/backend/domain/merchant/MerchantEntity.java
@@ -0,0 +1,42 @@
+package com.merchandise.backend.domain.merchant;
+
+import com.merchandise.backend.domain.product.ProductEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "merchant")
+public class MerchantEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String pinCode;
+
+    private String phoneNumber;
+
+    private String upiID;
+
+    @OneToMany(mappedBy = "merchantEntity", cascade = CascadeType.ALL)
+    private List<ProductEntity> productEntities = new ArrayList<>();
+}
